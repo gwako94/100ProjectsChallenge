a108198997c45ccd87fb57632781133ad3ecb411
gwako94 <galgallowako254@gmail.com>
2022-05-13 08:26:47 +0300

    band name generator

---
+++ band_generator.py
@@ -0,0 +1,8 @@
+#1. Concept input prompt and string concatenation.
+
+print("Welcome to the Band Name Generator")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)
